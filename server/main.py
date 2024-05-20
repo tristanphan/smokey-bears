02c91247370398ec2c0261f0d6b5d37c1df3e5c0
Tristan Phan <10486660+tristanphan@users.noreply.github.com>
2024-05-20 13:44:28 -0700

    [Server] Adapt web interface for project

--- server/main.py
+++ server/main.py
@@ -10,10 +10,10 @@ log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
 Temperature = float
-Humidity = float
+GasConcentration = float
 
 app = Flask(__name__)
-data: List[Tuple[date, Temperature, Humidity]] = []
+data: List[Tuple[date, Temperature, GasConcentration]] = []
 date_formatter = "%m/%d %H:%M:%S"  # previously "%H:%M:%S"
 
 
@@ -24,12 +24,17 @@ def portal():
 
 @app.route("/update", methods=["GET"])
 def update():
-    new_temperature = float(request.args["temperature"])
-    new_humidity = float(request.args["humidity"])
+    try:
+        new_temperature = float(request.args["temperature"])
+        new_gas_concentration = float(request.args["gas_concentration"])
+    except ValueError:
+        return "Invalid input, expected float", 400
     d = datetime.now()
-    data.append((d, new_temperature, new_humidity))
-    print(f"Updated temperature = {new_temperature} and humidity = {new_humidity} at {d.strftime(date_formatter)}")
-    return f"Set temperature = {new_temperature}, humidity = {new_humidity} at {d.strftime(date_formatter)}", 200
+    data.append((d, new_temperature, new_gas_concentration))
+    print(f"Updated temperature = {new_temperature} and gas concentration = {new_gas_concentration} at"
+          f" {d.strftime(date_formatter)}")
+    return (f"Set temperature = {new_temperature}, gas concentration = {new_gas_concentration} at"
+            f" {d.strftime(date_formatter)}"), 200
 
 
 @app.route("/retrieve", methods=["GET"])
@@ -37,7 +42,7 @@ def retrieve():
     return {
         "timestamps": [d.strftime(date_formatter) for d, _, _ in data],
         "temperature_values": [temp for _, temp, _ in data],
-        "humidity_values": [hum for _, _, hum in data],
+        "gas_concentration_values": [hum for _, _, hum in data],
     }, 200
 
 
@@ -48,10 +53,26 @@ def clear():
     return "Cleared all data", 200
 
 
-if __name__ == "__main__":
-    print(f"Starting server on port {int(sys.argv[1]) if len(sys.argv) > 1 else 5000}...")
+# noinspection HttpUrlsUsage
+def main():
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    host = "0.0.0.0"
+
+    print(f"Starting server at http://{host}:{port}/")
+
+    # Print instructions
+    print(f"Endpoints:")
+    print(f"    1. http://{host}:{port}/")
+    print(f"    2. http://{host}:{port}/update?temperature=0&gas_concentration=0")
+    print(f"    3. http://{host}:{port}/retrieve")
+    print(f"    4. http://{host}:{port}/clear")
+
     waitress.serve(
         app,
-        host="0.0.0.0",
+        host=host,
         port=int(sys.argv[1]) if len(sys.argv) > 1 else 5000,
     )
+
+
+if __name__ == "__main__":
+    main()
--- server/static/script.js
+++ server/static/script.js
@@ -1,46 +1,33 @@
 const ctx = document.getElementById("weather-chart").getContext("2d");
 const chart = new Chart(ctx, {
-        type: "line",
-        data: {
-            labels: [],
-            datasets: [
-                {
-                    label: "Temperature (°C)",
-                    data: [],
-                    backgroundColor: "rgba(255, 99, 132, 0.2)",
-                    borderColor: "rgba(255, 99, 132, 1)",
-                    borderWidth: 1,
-                    yAxisID: "temperature",
-                },
-                {
-                    label: "Humidity (%)",
-                    data: [],
-                    backgroundColor: "rgba(0, 0, 255, 0.2)",
-                    borderColor: "rgba(0, 0, 255, 1)",
-                    borderWidth: 1,
-                    yAxisID: "humidity",
-                },
-            ]
-        },
-        options: {
-            scales: {
-                temperature: {
-                    type: "linear",
-                    position: "left",
-                    display: true,
-                },
-                humidity: {
-                    type: "linear",
-                    position: "right",
-                    display: true,
-                    grid: {
-                        display: false,
-                    }
-                },
+    type: "line", data: {
+        labels: [], datasets: [{
+            label: "Temperature (°C)",
+            data: [],
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            yAxisID: "temperature",
+        }, {
+            label: "Gas Concentration (PPM)",
+            data: [],
+            backgroundColor: "rgba(0, 0, 255, 0.2)",
+            borderColor: "rgba(0, 0, 255, 1)",
+            borderWidth: 1,
+            yAxisID: "gas_concentration",
+        },]
+    }, options: {
+        scales: {
+            temperature: {
+                type: "linear", position: "left", display: true,
+            }, gas_concentration: {
+                type: "linear", position: "right", display: true, grid: {
+                    display: false,
+                }
             },
         },
     },
-);
+},);
 
 async function clearHistory() {
     if (confirm("Are you sure you want to clear the history?")) {
@@ -54,16 +41,16 @@ async function updateChart() {
     let data = await response.json()
     let timestamps = data["timestamps"];
     let temperatureValues = data["temperature_values"]
-    let humidityValues = data["humidity_values"];
+    let gasConcentrationValues = data["gas_concentration_values"];
     let numToShow = Math.max(0, document.getElementById("datapoint-count").value);
     if (numToShow > 0) {
         chart.data.labels = timestamps.slice(Math.max(0, timestamps.length - numToShow))
         chart.data.datasets[0].data = temperatureValues.slice(Math.max(0, temperatureValues.length - numToShow));
-        chart.data.datasets[1].data = humidityValues.slice(Math.max(0, humidityValues.length - numToShow));
+        chart.data.datasets[1].data = gasConcentrationValues.slice(Math.max(0, gasConcentrationValues.length - numToShow));
     } else {
         chart.data.labels = timestamps
         chart.data.datasets[0].data = temperatureValues
-        chart.data.datasets[1].data = humidityValues
+        chart.data.datasets[1].data = gasConcentrationValues
     }
     if (data["timestamps"].length === 0) {
         document.getElementById("avg_temp").innerText = "__";
@@ -74,11 +61,11 @@ async function updateChart() {
         document.getElementById("min_hum").innerText = "__";
     } else {
         document.getElementById("avg_temp").innerText = (data["temperature_values"].reduce((a, b) => a + b, 0) / data["temperature_values"].length).toFixed(2);
-        document.getElementById("avg_hum").innerText = (data["humidity_values"].reduce((a, b) => a + b, 0) / data["humidity_values"].length).toFixed(2);
+        document.getElementById("avg_hum").innerText = (data["gas_concentration_values"].reduce((a, b) => a + b, 0) / data["gas_concentration_values"].length).toFixed(2);
         document.getElementById("max_temp").innerText = (Math.max(...data["temperature_values"])).toFixed(2);
         document.getElementById("min_temp").innerText = (Math.min(...data["temperature_values"])).toFixed(2);
-        document.getElementById("max_hum").innerText = (Math.max(...data["humidity_values"])).toFixed(2);
-        document.getElementById("min_hum").innerText = (Math.min(...data["humidity_values"])).toFixed(2);
+        document.getElementById("max_hum").innerText = (Math.max(...data["gas_concentration_values"])).toFixed(2);
+        document.getElementById("min_hum").innerText = (Math.min(...data["gas_concentration_values"])).toFixed(2);
     }
     chart.update();
 
@@ -86,7 +73,12 @@ async function updateChart() {
     document.getElementById("last_refresh_time").innerText = now.toLocaleDateString() + ", " + now.toLocaleTimeString()
 }
 
-updateChart()
+void updateChart()
 
 // Poll for information to update
 setInterval(updateChart, 200)
+
+window.addEventListener("resize", () => {
+    chart.resize();
+});
+
